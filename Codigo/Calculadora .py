637c9bfd13c74093f888e7685792dd3d3c346915
Santiago2132 <santiagomaldonadogamer@gmail.com>
2022-04-05 13:29:48 -0500

    Prototipo de calculadora Simple

---
+++ Codigo/Calculadora .py
@@ -0,0 +1,37 @@
+#Funciones 
+def suma (a,b):
+    result=a+b
+    print("La suma de ",a," y ",b," es: ",result)
+def resta (a,b):
+    result=a-b
+    print("La resta de ",a," y ",b," es: ",result)
+def multiplicación (a,b):
+    result=a*b
+    print("La multipliación de ",a," y ",b," es: ",result)
+def division(a,b):
+    result=a/b
+    print("La division de ",a," y ",b," es: ",result)
+#Programa 
+print("Bienvenido :D")
+while True:
+    print("Opciones \n1)Suma de dos numeros \n2)Resta de dos numeros \n3)Multipliación de dos numeros \n4)División de dos numeros \n(Cualquier otro numero o letra para apagar)")
+    option=int(input("Ingrese la opción: "))
+    if(option==1):
+        num1=int(input("Ingrese un número: "))
+        num2=int(input("Ingrese otro numero: "))
+        suma(num1,num2)
+    elif(option==2):
+        num1=int(input("Ingrese un número: "))
+        num2=int(input("Ingrese otro numero: "))
+        resta(num1,num2)
+    elif(option==3):
+        num1=int(input("Ingrese un número: "))
+        num2=int(input("Ingrese otro numero: "))
+        multiplicación(num1,num2)
+    elif(option==4):
+        num1=int(input("Ingrese un número: "))
+        num2=int(input("Ingrese el número popr el cual se va a dividir: "))
+        division(num1,num2)
+    else:
+        print("Good bye")
+        break
